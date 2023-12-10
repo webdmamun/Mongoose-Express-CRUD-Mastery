46b71d82aea5993a8a2d11e0458dbb669ba18c72
MAMUN <mamun@MAMUNs-MacBook-Pro.local>
2023-12-10 12:26:48 +0000

    project-setup

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+import cors from 'cors';
+
+//persers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+// app.use('/api', UserRoutes)
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Wow! Mongoose Express CRUD Mastery is Running',
+  });
+});
+
+export default app;
